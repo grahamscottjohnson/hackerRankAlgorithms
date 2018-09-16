7ac35ef1a98ff0990c55e238edb6fb0386df1932
Graham Johnson <grahamscottjohnson@gmail.com>
2018-09-16 12:17:48 -0400

    reworked searchInSortedMatrix and found error in method

--- searchInSortedMatrix.js
+++ searchInSortedMatrix.js
@@ -1,12 +1,45 @@
 function searchInSortedMatrix(matrix, target) {
   //row is up and down
-  if (!matrix.length || !matrix[0].length) return [-1, -1];
-  let row = 0;
-  let col = 0;
-  if (matrix[row][col] === target) {
-    return [row, col];
+  // if (isSmall(matrix)) return naiveSearch(matrix, target);
+  let minRow = 0;
+  let minCol = 0;
+  let maxRow = matrix.length - 1;
+  let maxCol = matrix[0].length - 1;
+  while (!isSmall()) {
+    const midRow = midPoint(minRow, maxRow);
+    const midCol = midPoint(minCol, maxCol);
+    const value = matrix[midRow][midCol];
+    if (value === target) {
+      return [midRow, midCol];
+    } else if (target < value) {
+      maxRow = midRow;
+      maxCol = midCol;
+    } else {
+      minRow = midRow;
+      minCol = midCol;
+    }
   }
-  return [-1, -1];
+  return naiveSearch();
+
+  function isSmall() {
+    return maxRow - minRow < 2 && maxCol - minCol < 2;
+  }
+
+  function naiveSearch() {
+    // console.log(minRow, minCol, maxRow, maxCol);
+    for (let i = minRow; i <= maxRow; i += 1) {
+      for (let j = minCol; j <= maxCol; j += 1) {
+        if (matrix[i][j] === target) {
+          return [i, j];
+        }
+      }
+    }
+    return [-1, -1];
+  }
+}
+
+function midPoint(start, end) {
+  return Math.ceil((end + start) / 2);
 }
 
 // Do not edit the line below.
--- test/searchInSortedMatrix.spec.js
+++ test/searchInSortedMatrix.spec.js
@@ -18,6 +18,33 @@ describe.only('searchInSortedMatrix', () => {
     const matrix = [row1, row2];
     expect(searchInSortedMatrix(matrix, 3)).to.deep.equal([1, 1]);
   });
+  it('returns [0,1] for a 2x2 matrix', () => {
+    const row1 = [1, 3];
+    const row2 = [2, 4];
+    const matrix = [row1, row2];
+    expect(searchInSortedMatrix(matrix, 3)).to.deep.equal([0, 1]);
+  });
+  it('works for a 3x3 matrix', () => {
+    const row1 = [1, 3, 5];
+    const row2 = [2, 4, 6];
+    const row3 = [7, 8, 9];
+    const matrix = [row1, row2, row3];
+    expect(searchInSortedMatrix(matrix, 1)).to.deep.equal([0, 0]);
+    expect(searchInSortedMatrix(matrix, 4)).to.deep.equal([1, 1]);
+    expect(searchInSortedMatrix(matrix, 6)).to.deep.equal([1, 2]);
+    expect(searchInSortedMatrix(matrix, 10)).to.deep.equal([-1, -1]);
+  });
+  it('works for a 3x5 matrix', () => {
+    const row1 = [1, 3, 5, 11, 12];
+    const row2 = [2, 4, 6, 13, 14];
+    const row3 = [7, 8, 9, 15, 16];
+    const matrix = [row1, row2, row3];
+    expect(searchInSortedMatrix(matrix, 12)).to.deep.equal([0, 4]);
+    expect(searchInSortedMatrix(matrix, 8)).to.deep.equal([2, 1]);
+    expect(searchInSortedMatrix(matrix, 6)).to.deep.equal([1, 2]);
+    expect(searchInSortedMatrix(matrix, 10)).to.deep.equal([-1, -1]);
+  });
   //what to do if there are two targets in matrix?
+  //return any matching index
   afterEach(() => {});
 });
