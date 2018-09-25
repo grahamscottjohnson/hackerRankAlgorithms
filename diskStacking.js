38840d4ba3e7e0a5e6ff7e603dd82ef82168ac91
Graham Johnson <grahamscottjohnson@gmail.com>
2018-09-25 09:12:01 -0400

    tried a dotProduct projection onto the identity line, seems too complicated

--- diskStacking.js
+++ diskStacking.js
@@ -1,6 +1,39 @@
 //https://www.algoexpert.io/questions/Disk%20Stacking
 
+class Disk {
+  constructor(array) {
+    this.dimensions = array;
+  }
+
+  get projectedUnitValue() {
+    //111 dot dimensions
+    const dotProduct = this.dotProductWithIdentity();
+    // const magnitude = this.magnitude();
+    // const angle = Math.acos(magnitude / dotProduct);
+    return dotProduct;
+  }
+
+  dotProductWithIdentity() {
+    return this.dimensions.reduce(sum);
+
+    function sum(acc, cur) {
+      return acc + cur;
+    }
+  }
+
+  magnitude() {
+    const sumOfSquares = this.dimensions.reduce((acc, cur) => {
+      return acc + cur * cur;
+    }, 0);
+    return Math.sqrt(sumOfSquares);
+  }
+}
+
 function diskStacking(disks) {
+  //project onto y = x = z
+  //sort based on this projection (handle collisions based on projection)
+  //traverse the data with a use / don't use aggregation
+  //return that
   const sortedDisks = disks.slice().sort((a, b) => a[0] - b[0]);
   return filterDuplicates(sortedDisks);
 }
@@ -21,3 +54,4 @@ function unsquishDisk(string) {
 
 // Do not edit the line below.
 exports.diskStacking = diskStacking;
+exports.Disk = Disk;
--- test/diskStacking.spec.js
+++ test/diskStacking.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { diskStacking } = require('../diskStacking');
+const { diskStacking, Disk } = require('../diskStacking');
 
 describe.only('diskStacking', () => {
   beforeEach(() => {});
@@ -9,13 +9,13 @@ describe.only('diskStacking', () => {
     expect(diskStacking(inputDisks)).to.deep.equal(expectedDisks);
   });
   it('works for two unsorted disks one variable', () => {
-    const inputDisks = [[2, 0, 0], [1, 0, 0]];
-    const expectedDisks = [[1, 0, 0], [2, 0, 0]];
+    const inputDisks = [[2, 1, 1], [1, 0, 0]];
+    const expectedDisks = [[1, 0, 0], [2, 1, 1]];
     expect(diskStacking(inputDisks)).to.deep.equal(expectedDisks);
   });
   it('works for three disks one variable', () => {
-    const inputDisks = [[2, 0, 0], [3, 0, 0], [1, 0, 0]];
-    const expectedDisks = [[1, 0, 0], [2, 0, 0], [3, 0, 0]];
+    const inputDisks = [[2, 1, 1], [3, 2, 2], [1, 0, 0]];
+    const expectedDisks = [[1, 0, 0], [2, 1, 1], [3, 2, 2]];
     expect(diskStacking(inputDisks)).to.deep.equal(expectedDisks);
   });
   it('works for three disks one variable of equal size', () => {
@@ -24,4 +24,21 @@ describe.only('diskStacking', () => {
     expect(diskStacking(inputDisks)).to.deep.equal(expectedDisks);
   });
   afterEach(() => {});
+});
+
+describe.only('Disk', () => {
+  describe('projectedUnitValue', () => {
+    beforeEach(() => {});
+    it('returns .5 for [1,1,0]', () => {
+      const inputDisk = new Disk([1, 1, 0]);
+      const expectedValue = 0.5;
+      expect(inputDisk.projectedUnitValue).to.deep.equal(expectedValue);
+    });
+    it('returns -.5 for [0,-1,-1]', () => {
+      const inputDisk = new Disk([0, -1, -1]);
+      const expectedValue = -0.5;
+      expect(inputDisk.projectedUnitValue).to.deep.equal(expectedValue);
+    });
+    afterEach(() => {});
+  });
 });
